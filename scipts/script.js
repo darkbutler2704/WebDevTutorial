dc43fff67b4d12ad83e282e5241e3d07c7226cb0
admin <cabuangtg@students.national-u.edu.ph>
2022-12-23 19:27:12 +0800

    Initial commit

---
+++ scipts/script.js
@@ -0,0 +1,7 @@
+function helloHover() {
+    document.getElementById("hello").innerHTML = "Ooops!!!!"
+}
+
+function exitHover() {
+    document.getElementById("hello").innerHTML = "Hello, Welcome To World!"
+}
